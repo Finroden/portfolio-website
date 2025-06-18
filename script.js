22b998bba76e4326dddca54931ebbf48077bc213
adrianczarnecki <adrianczarnecki25@gmail.com>
2025-06-18 15:11:18 +0200

    Load Font Awesome asynchronously to improve performance

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+const link = document.createElement("link");
+link.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css";
+link.rel = "stylesheet";
+document.head.appendChild(link);
